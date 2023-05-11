20030ac5a4f457d8784208e918878126413fad37
Ahnwonseok <95980876+Ahnwonseok@users.noreply.github.com>
2023-05-11 16:11:52 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,59 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/app/deploy.log
+PROJECT_ROOT="/home/ubuntu/app"
+JAR_FILE="$PROJECT_ROOT/chaebbiSpring.jar"
 
-echo ">>> build 파일 복사" >> /home/ubuntu/app/deploy.log
-DEPLOY_PATH=/home/ubuntu/app/
-cp $BUILD_JAR $DEPLOY_PATH
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+TIME_NOW=$(date +%c)
 
-if [ -z $CURRENT_PID ]
-then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
-else
-  echo ">>> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/app/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/app/deploy.log 2>/home/ubuntu/app/deploy_err.log &
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-
-
-# #!/usr/bin/env bash
-
-# REPOSITORY=/home/ubuntu/app
-
-# echo "> 현재 구동 중인 애플리케이션 pid 확인"
-
-# CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
-
-# echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
-# if [ -z "$CURRENT_PID" ]; then
-#   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-# else
-#   echo "> kill -15 $CURRENT_PID"
-#   kill -15 $CURRENT_PID
-#   sleep 5
-# fi
-
-# echo "> 새 애플리케이션 배포"
-
-# JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
-
-# echo "> JAR NAME: $JAR_NAME"
-
-# echo "> $JAR_NAME 에 실행권한 추가"
-
-# chmod +x $JAR_NAME
-
-# echo "> $JAR_NAME 실행"
-
-# nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
